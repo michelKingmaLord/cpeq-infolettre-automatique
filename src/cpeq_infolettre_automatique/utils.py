e4914e8607e68f9e90748236163c525db7eb5148
michelKingmaLord <0801342@csfoy.ca>
2024-05-07 10:53:55 -0400

    style(utils): addition of correct docstrings and typing of various values and functions

--- src/cpeq_infolettre_automatique/utils.py
+++ src/cpeq_infolettre_automatique/utils.py
@@ -1,9 +1,17 @@
 """Utility functions for processing and saving data."""
 import json
+from typing import Any
 
 
-def process_raw_response(raw_response: str) -> list:
-    """Converts raw JSON lines into a list of dictionaries (valid JSON array)."""
+def process_raw_response(raw_response: str) -> list[dict[str, Any]] | dict[str, str]:
+    """Convert raw JSON lines into a list of dictionaries (valid JSON array) or return an error message.
+
+    Args:
+        raw_response (str): Raw response data in JSON lines format.
+
+    Returns:
+        list[dict[str, Any]] | dict[str, str]: List of dictionaries if successful, else an error message.
+    """
     try:
         data = [json.loads(line) for line in raw_response.strip().split("\n") if line.strip()]
     except json.JSONDecodeError as error:
@@ -12,8 +20,16 @@ def process_raw_response(raw_response: str) -> list:
         return data
 
 
-def save_data_to_json(data: list, file_path: str ="output.json") -> str:
-    """Saves processed data to a JSON file."""
+def save_data_to_json(data: list, file_path: str = "output.json") -> str:
+    """Saves processed data to a JSON file.
+
+    Args:
+        data (list): Processed data to be saved.
+        file_path (str): Path where the JSON data will be saved.
+
+    Returns:
+        str: A success message or an error message.
+    """
     try:
         with open(file_path, "w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
